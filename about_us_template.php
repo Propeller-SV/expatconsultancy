9f52b1d67c0ce1cd3473ebcbc4c842abf19128b4
Serhii Prokhorenko <grey.psv@gmail.com>
2015-08-05 09:37:25 +0300

    use custom_image instead of post_thumbnail on about_us_page. Use the_content instead of echo_get_the_content

--- about_us_template.php
+++ about_us_template.php
@@ -11,29 +11,22 @@
 		<div class="row">
 			<div class="col-sm-12">
 				<div class="item banner">
-					<?php
-						while ( have_posts() ) : the_post();
-							?>
-							<!-- display featured image -->
-							<?php $img = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_id()), 'full')[0];?>
-							<img class="img-responsive" src="<?php if ($img) echo $img; else echo IMAGES . '/brand-foto.png';?>" alt="...">
-							<!-- display excerpt -->
-							<div class="carousel-caption">
-								<h6 class="hidden-sm hidden-md hidden-lg"><?php echo get_the_excerpt();?> </h6>
-								<h1 class="hidden-xs"><?php echo get_the_excerpt();?> </h1>
-							</div>
-							<?php
-						endwhile;
-						?>
-					</div>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<!-- display custom image -->
+						<?php $brand = get_post_meta( get_the_id(), 'brand_photo_attachment', true ); ?>
+						<img class="img-responsive" src="<?php if ($brand) echo $brand; else echo IMAGES . '/brand-foto.png'; ?>" alt="...">
+						<!-- display excerpt -->
+						<div class="carousel-caption">
+							<h6 class="hidden-sm hidden-md hidden-lg"><?php echo get_the_excerpt();?></h6>
+							<h1 class="hidden-xs"><?php echo get_the_excerpt();?></h1>
+						</div>
+					<?php endwhile;	?>
+				</div>
 			</div>
 			<div class="col-sm-12">
-				<div class="background-color">
+				<div class="background-color content-text">
 					<!-- display content -->
-					<?php
-					while ( have_posts() ) : the_post();
-						?><p class="content-text"><?php echo get_the_content();?> </p><?php
-					endwhile;
-					?>
-
+					<?php while ( have_posts() ) : the_post(); ?>
+						<?php the_content();?>
+					<?php endwhile; ?>
 <?php get_footer(); ?>
