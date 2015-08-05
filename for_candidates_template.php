35b75895633371e9a1fbca6decbd94f286e1d56e
Serhii Prokhorenko <grey.psv@gmail.com>
2015-08-05 09:41:11 +0300

    use the_content instead of echo_get_the_content on for_candidates_template

--- for_candidates_template.php
+++ for_candidates_template.php
@@ -5,33 +5,25 @@
 ?>
 
 <?php get_header(); ?>
-
 <div class="forcandidates-content content">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
-				<div class="background-color">
-					<?php
-					while ( have_posts() ) : the_post();
-						?><p class="content-text"><?php echo get_the_content();?> </p>
-							<ol>
-								<?php $doc = get_post_meta(get_the_ID(), 'expat_doc_attachment', true); ?>
-								<?php
-									if (isset($doc[1])) {
-										for ($i=1; $i < count($doc); $i++) {
-											$filename = pathinfo($doc[$i]['file'])['filename'];
-											$extension = pathinfo($doc[$i]['file'])['extension'];
-											$url = $doc[$i]['url'];
-											?>
-												<li>
-													<p><?php echo $filename . '&nbsp; &nbsp; &nbsp;' . $extension . __(' file ', 'expatconsultany'); ?><a href="<?php echo $url; ?>" role="button"><?php _e('Download', 'expatconsultany') ?></a></p>
-												</li>
-											<?php
-										}
-									}
-								?>
-							</ol><?php
-					endwhile;
-					?>
-
+				<div class="background-color content-text">
+					<?php while ( have_posts() ) : the_post(); ?>
+						<?php the_content(); ?>
+					<?php endwhile;	?>
+					<?php $doc = get_post_meta(get_the_ID(), 'expat_doc_attachment', true); ?>
+					<?php if (isset($doc[1])): ?>
+						<ol>
+						<?php for ($i=1; $i < count($doc); $i++): ?>
+							<?php $filename = pathinfo($doc[$i]['file'])['filename']; ?>
+							<?php $extension = pathinfo($doc[$i]['file'])['extension']; ?>
+							<?php $url = $doc[$i]['url']; ?>
+								<li>
+									<p><?php echo $filename . '&nbsp; &nbsp; &nbsp;' . $extension . __(' file ', 'expatconsultany'); ?><a href="<?php echo $url; ?>" role="button"><?php _e('Download', 'expatconsultany') ?></a></p>
+								</li>
+						<?php endfor; ?>
+						</ol>
+					<?php endif; ?>
 <?php get_footer(); ?>
